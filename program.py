f52ccdeb6abe779a84dd2101bfa11999afb7b8bd
George <georgewaller@gswdh.co.uk>
2024-04-06 16:05:38 +0100

    Programming possible from the UI

---
+++ program.py
@@ -0,0 +1,17 @@
+import xsdb
+session = xsdb.start_debug_session()
+session.connect(url="TCP:127.0.0.1:3121")
+
+# Add the ARM core to the jtag connection
+#jtag = session.jtag_target(filter='name==arm_dap')
+
+# Download FSBL to A9 #0, block untill FSBL completes
+a9_0 = session.targets("--set", filter="name =~ *MPCore #0*")
+session.rst(type='cores')
+a9_0.dow("camera_platform/zynq_fsbl/build/fsbl.elf")
+#a9_0.con("--block", timeout=3)
+a9_0.con()
+
+# Download hello world to A53 #0
+a9_0.dow("camera_program/build/camera_program.elf")
+a9_0.con()
