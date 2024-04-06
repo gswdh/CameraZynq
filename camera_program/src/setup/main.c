ef7ba0f28673e17936b9fc570254631ddc264d53
George <georgewaller@gswdh.co.uk>
2024-04-06 18:54:22 +0100

    Reading registers from the ethernet phy

--- camera_program/src/setup/main.c
+++ camera_program/src/setup/main.c
@@ -14,10 +14,26 @@
 #include "uart.h"
 #include "usb.h"
 
+#include "xemacps.h"
+
+static void to_binary16(uint16_t number, char *output)
+{
+	int j = 0; // Output array index
+	for (int i = 15; i >= 0; i--)
+	{
+		uint16_t bit = (number >> i) & 1;
+		output[j++] = bit + '0'; // Convert bit to character and store in output
+		if (i % 4 == 0 && i != 0)
+		{ // Add a space after every 4 bits, except at the end
+			output[j++] = ' ';
+		}
+	}
+	output[j] = '\0'; // Null terminator
+}
+
 static void timer_heartbeat_cb(TimerHandle_t pxTimer)
 {
 	(void)pxTimer;
-	xil_printf("Camera heartbeat. Tick = %ums.\r\n", xTaskGetTickCount());
 
 	// Flash the LED
 	gpio_toggle(FPGA_LED_0);
@@ -39,8 +55,24 @@ int main(void)
 												 pdTRUE,
 												 NULL,
 												 timer_heartbeat_cb);
-
 	xTimerStart(timer_heartbeat, 0);
+
+	// XEmacPs_Config *emac_config = XEmacPs_LookupConfig(XPAR_XEMACPS_0_BASEADDR);
+	// XEmacPs emac = {0};
+	// XEmacPs_CfgInitialize(&emac, emac_config, XPAR_XEMACPS_0_BASEADDR);
+
+	// xil_printf("\r\n\r\n");
+
+	// char bin_num[21] = {0};
+	// for (uint32_t addr = 0; addr < 32; addr++)
+	// {
+	// 	uint16_t data = 0;
+	// 	XEmacPs_PhyRead(&emac, 0x01, addr, &data);
+	// 	to_binary16(data, bin_num);
+	// 	xil_printf("PHY REGS: reg = 0x%02x, reg value = 0x%04x or %s\r\n", addr, data, bin_num);
+	// }
+
+	// xil_printf("\r\n\r\n");
 
 	/* Start the tasks and timer running. */
 	vTaskStartScheduler();
