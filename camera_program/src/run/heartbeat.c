47bbfd32bd9d6959627cb835fe515759cfbdd05a
George <georgewaller@gswdh.co.uk>
2024-04-07 08:37:34 +0100

    main simplified and heartbeat moved to it's on source

---
+++ camera_program/src/run/heartbeat.c
@@ -0,0 +1,32 @@
+#include "heartbeat.h"
+
+#include <stdint.h>
+#include <stdbool.h>
+
+/* FreeRTOS includes. */
+#include "FreeRTOS.h"
+#include "task.h"
+#include "queue.h"
+#include "timers.h"
+
+#include "xil_printf.h"
+
+#include "gpio.h"
+
+static void heartbeat_task(TimerHandle_t pxTimer)
+{
+    (void)pxTimer;
+    gpio_toggle(FPGA_LED_0);
+}
+
+void heartbeat_start()
+{
+    // Start the task
+    TimerHandle_t timer = xTimerCreate((const char *)"Heartbeat Timer",
+                                       pdMS_TO_TICKS(1000),
+                                       pdTRUE,
+                                       NULL,
+                                       heartbeat_task);
+
+    xTimerStart(timer, 0);
+}
--- camera_program/src/setup/main.c
+++ camera_program/src/setup/main.c
@@ -17,67 +17,17 @@
 
 /* Program tasks */
 #include "shutter_button.h"
-
-// #include "xemacps.h"
-
-// static void to_binary16(uint16_t number, char *output)
-// {
-// 	int j = 0; // Output array index
-// 	for (int i = 15; i >= 0; i--)
-// 	{
-// 		uint16_t bit = (number >> i) & 1;
-// 		output[j++] = bit + '0'; // Convert bit to character and store in output
-// 		if (i % 4 == 0 && i != 0)
-// 		{ // Add a space after every 4 bits, except at the end
-// 			output[j++] = ' ';
-// 		}
-// 	}
-// 	output[j] = '\0'; // Null terminator
-// }
-
-static void timer_heartbeat_cb(TimerHandle_t pxTimer)
-{
-	(void)pxTimer;
-
-	// Flash the LED
-	gpio_toggle(FPGA_LED_0);
-
-	// Send some dummy data to the PMC
-	char *msg = "somedata\n";
-	uart_tx((uint8_t *)msg, (uint32_t)strlen(msg));
-}
+#include "heartbeat.h"
 
 int main(void)
 {
 	gpio_init();
 	uart_init();
 	spi_init();
-
 	// usb_init();
 
-	TimerHandle_t timer_heartbeat = xTimerCreate((const char *)"Heartbeat Timer",
-												 pdMS_TO_TICKS(1000),
-												 pdTRUE,
-												 NULL,
-												 timer_heartbeat_cb);
-	xTimerStart(timer_heartbeat, 0);
-
-	// XEmacPs_Config *emac_config = XEmacPs_LookupConfig(XPAR_XEMACPS_0_BASEADDR);
-	// XEmacPs emac = {0};
-	// XEmacPs_CfgInitialize(&emac, emac_config, XPAR_XEMACPS_0_BASEADDR);
-
-	// xil_printf("\r\n\r\n");
-
-	// char bin_num[21] = {0};
-	// for (uint32_t addr = 0; addr < 32; addr++)
-	// {
-	// 	uint16_t data = 0;
-	// 	XEmacPs_PhyRead(&emac, 0x01, addr, &data);
-	// 	to_binary16(data, bin_num);
-	// 	xil_printf("PHY REGS: reg = 0x%02x, reg value = 0x%04x or %s\r\n", addr, data, bin_num);
-	// }
-
-	// xil_printf("\r\n\r\n");
+	/* Simple blink to show we're alive */
+	heartbeat_start();
 
 	/* Start program tasks */
 	shutter_button_start();
