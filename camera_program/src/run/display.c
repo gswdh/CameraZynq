7e95a38bc1a8bd328dad9bd655ee57bd10b114a0
George <georgewaller@gswdh.co.uk>
2024-05-19 20:01:41 +0100

    But

--- camera_program/src/run/display.c
+++ camera_program/src/run/display.c
@@ -21,66 +21,66 @@
 
 void ssd_spi_write(uint8_t *data, uint32_t len)
 {
-    for (uint32_t i = 0; i < len; i++)
-    {
-        spi_1_tx(data + i, 1);
-    }
+	for (uint32_t i = 0; i < len; i++)
+	{
+		spi_1_tx(data + i, 1);
+	}
 }
 
 void ssd_set_dc(bool en)
 {
-    gpio_write(DSP_D_NC, en);
+	gpio_write(DSP_D_NC, en); // This
 }
 
 void ssd_set_cs(bool en)
 {
-    gpio_write(DSP_SPI_NCS, en);
+	gpio_write(DSP_SPI_NCS, en);
 }
 
 void ssd_set_rst(bool en)
 {
-    gpio_write(DSP_NRESET, en);
+	gpio_write(DSP_NRESET, en);
 }
 
 void ssd_pwr_cont(bool en)
 {
-    gpio_write(PWR_VDISP_EN, en);
+	gpio_write(PWR_VDISP_EN, en);
 }
 
 void ssd_delay_ms(uint32_t time_ms)
 {
-    vTaskDelay(pdMS_TO_TICKS(time_ms));
+	vTaskDelay(pdMS_TO_TICKS(time_ms));
 }
 
 static uint8_t display_data[SSD_DISP_BUFFER_LEN] = {100};
 
 static void display_task(void *params)
 {
-    (void *)params;
+	(void *)params;
 
-    while (true)
-    {
-        if (dmgui_update_needed())
-        {
-            ssd_update_display(display_data);
+	while (true)
+	{
+		if (dmgui_update_needed())
+		{
+			ssd_update_display(display_data);
 
-            dmgui_update_done();
-        }
+			dmgui_update_done();
+		}
 
-        ssd_delay_ms(10);
-    }
+		ssd_delay_ms(10);
+	}
 
-    vTaskDelete(NULL);
+	vTaskDelete(NULL);
 }
 
 void display_start()
 {
-    /* Init the display HW */
-    ssd_init();
+	/* Init the display HW */
+	ssd_init();
 
-    /* Init the GUI */
-    dmgui_init(display_data);
+	/* Init the GUI */
+	dmgui_init(display_data);
 
-    /* Start the task */
-    xTaskCreate(display_task, "Display Task", 4096, NULL, tskIDLE_PRIORITY, NULL);
+	/* Start the task */
+	xTaskCreate(display_task, "Display Task", 4096, NULL, tskIDLE_PRIORITY, NULL);
 }
